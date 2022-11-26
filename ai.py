d1b90b30d1d3165d57d50c064097fbd9d646b2c6
Matthew Grady <matthewbgrady90@gmail.com>
2022-11-25 23:28:13 -0500

    canny edge support

--- ai.py
+++ ai.py
@@ -3,7 +3,6 @@ from PyQt5 import QtCore, QtWidgets, QtGui
 
 ## Other Required Imports
 import time
-import asyncio
 import threading
 
 ## Image Processing Imports
@@ -11,7 +10,6 @@ import win32gui
 import cv2 as cv
 from matplotlib import pyplot
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-import pyautogui
 import pytesseract
 from PIL import Image, ImageGrab, ImageEnhance, ImageFilter
 
@@ -23,15 +21,13 @@ import wmi
 ## Image Processing Stereo
 stereo = cv.StereoBM_create(numDisparities = 128,
                             blockSize = 5)
-print(stereo.getSpeckleRange())
-stereo.setSpeckleRange(1)
-print(stereo.getSpeckleRange())
 
 # A class that extends the Thread class
 class VisionThread(threading.Thread):
 
     def __init__(self, fps, process, event, figure, axe):
        threading.Thread.__init__(self)
+       self.type = 'Canny Edge'
        self.figure = figure
        self.axe = axe
        self.fps = fps
@@ -41,37 +37,63 @@ class VisionThread(threading.Thread):
 
     def run(self):
         while True:
-            if self.active.is_set():
-                break
+            if self.type == 'Stereo':
+                if self.active.is_set():
+                    break
 
-            print("I see fine.")
+                try:
+                    win32gui.SetForegroundWindow(self.process)
+                except:
+                    pass
+                finally:
+                    pass
 
-            print(win32gui.GetWindowDC(self.process))
-            try:
-                win32gui.SetForegroundWindow(self.process)
-            except:
-                pass
-            finally:
-                pass
+                bbox = win32gui.GetWindowRect(self.process)
 
-            bbox = win32gui.GetWindowRect(self.process)
+                leftEye = cv.cvtColor(np.asarray(ImageGrab.grab(bbox)), cv.COLOR_RGB2GRAY)
+                # 1 Second Divided by the Expected Frames Per Second
+                time.sleep(1 / self.fps)
 
-            leftEye = cv.cvtColor(np.asarray(ImageGrab.grab(bbox)), cv.COLOR_RGB2GRAY)
-            # 1 Second Divided by the Expected Frames Per Second
-            time.sleep(1 / self.fps)
+                rightEye = cv.cvtColor(np.asarray(ImageGrab.grab(bbox)), cv.COLOR_RGB2GRAY)
 
-            rightEye = cv.cvtColor(np.asarray(ImageGrab.grab(bbox)), cv.COLOR_RGB2GRAY)
-            print(rightEye)
-            # 1 Second Divided by the Expected Frames Per Second
-            time.sleep(1 / self.fps)
+                # 1 Second Divided by the Expected Frames Per Second
+                time.sleep(1 / self.fps)
 
-            # Calculate Disparity
-            disparity = stereo.compute(leftEye, rightEye)
+                # Calculate Disparity
+                disparity = stereo.compute(leftEye, rightEye)
 
-            # DEBUG
-            self.axe.clear()
-            self.axe.imshow(disparity)
-            self.figure.draw_idle()
+                # DEBUG
+                self.axe.clear()
+                self.axe.imshow(disparity)
+                self.figure.draw_idle()
+
+            if self.type == 'Canny Edge':
+                if self.active.is_set():
+                    break
+
+                try:
+                    win32gui.SetForegroundWindow(self.process)
+                except:
+                    pass
+                finally:
+                    pass
+
+                bbox = win32gui.GetWindowRect(self.process)
+
+                eyes = cv.cvtColor(np.asarray(ImageGrab.grab(bbox)), cv.COLOR_RGB2GRAY)
+                # 1 Second Divided by the Expected Frames Per Second
+                time.sleep(1 / self.fps)
+
+                # Blur the image
+                img_blur = cv.GaussianBlur(eyes, (3,3), 0)
+
+                # Canny Edge Detection
+                edges = cv.Canny(image=img_blur, threshold1=100, threshold2=200)
+
+                # Display
+                self.axe.clear()
+                img = self.axe.imshow(edges, alpha=0.6)
+                self.figure.draw_idle()
 
 class Overlay(QtWidgets.QWidget):
 
@@ -178,26 +200,26 @@ class Overlay(QtWidgets.QWidget):
         self.active.set()
         time.sleep(3)
         self.active.clear()
-        axe = self.preview.axes.add_subplot(111)
-        VisionThread(fps=30, process=processID, event=self.active, figure=self.preview, axe=axe)
+        axe = self.preview.axes.add_subplot(111, facecolor="none")
+        VisionThread(fps=5, process=processID, event=self.active, figure=self.preview, axe=axe)
         time.sleep(10)
         pyplot.show()
 
 
     def keyPressEvent(self, event):
-        print(event.modifiers())
-        print(event)
         if event.key() == QtCore.Qt.Key_Escape:
             self.active.set()
             self.close()
-            exit()
+            quit()
 
 class QTCanvas(FigureCanvasQTAgg):
 
-    def __init__(self, parent=None, width=10, height=8, dpi=100):
+    def __init__(self, parent=None, width=40, height=32, dpi=100):
         figure = pyplot.Figure(figsize=(width, height), dpi=dpi)
+        figure.patch.set_alpha(0.33)
         self.axes = figure
         super(QTCanvas, self).__init__(figure)
+        self.setStyleSheet("background-color:transparent;")
 
 
 if __name__ == '__main__':
