f5ae4bf4a0c20a299c3b443b06a8eb43085fda89
Matthew Grady <matthewbgrady90@gmail.com>
2023-09-17 20:15:39 -0400

    Updated

---
+++ programs/base.py
@@ -0,0 +1,73 @@
+from collections.abc import Callable
+
+class BaseProgram(object):
+    """
+    Base Program designed to be extended.add()
+
+    A program, basically, orders jobs based on priority.
+    Looks for a job that matches the requirements
+    """
+    _jobs = []
+    _current_index = 0
+
+    @property
+    def jobs(self):
+        return self._jobs
+
+    @jobs.setter
+    def jobs(
+        self,
+        is_valid: Callable[[], bool],
+        callback: Callable[[], bool],
+        priority: int = 0,
+    ):
+        """
+        Setter for the Job Object
+
+        Args:
+            is_valid (Callable[[], bool]): Is Valid? Returns bool
+            callback (Callable[[], bool]): If Valid Trigger Callback
+            priority (int): Priority of the job >= 0
+        """
+        self._jobs.append(
+            Job(
+                is_valid,
+                callback,
+                priority
+            )
+        )
+        self._jobs.sort(key = lambda x: x["priority"])
+        self._notify_observers("jobs", current)
+
+
+    ### Call Back / Observer Section
+    def _notify_observers(self, prop, current):
+        """
+        Notify Observer Callbacks
+        """
+        if prop in self._observer_callbacks:
+            observers = self._observer_callbacks[prop]
+            if isinstance(observers, list):
+                for callback in observers:
+                    callback(current)
+
+    def register_callback(self, prop, callback):
+        """
+        Register Observer Callbacks
+        """
+        if prop in self._observer_callbacks:
+            observers = self._observer_callbacks[prop]
+            if isinstance(observers, list):
+                observers.append(callback)
+
+class Job(object):
+
+    def __init__(
+        self,
+        is_valid: Callable[[], bool],
+        callback: Callable[[], bool],
+        priority: int = 0,
+    ):
+        self._priority = priority
+        self._is_valid = is_valid
+        self._callback = callback
